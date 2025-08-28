0aa8752eeae765b34aeba5f61abf21ac4f7a5a3d
Francesco <francesco.menghetti@gmail.com>
2025-08-28 19:56:20 +0200

    Migration to KMP
    - Android UI

--- android/src/androidMain/kotlin/dev/t0rzz/samloaderreloaded/MainActivity.kt
+++ android/src/androidMain/kotlin/dev/t0rzz/samloaderreloaded/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import app.samloader.common.Api
+import dev.t0rzz.samloaderreloaded.ui.DuofrostApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Text(text = Api.appName() + " (Android) — UI to be implemented.")
+                DuofrostApp()
             }
         }
     }
